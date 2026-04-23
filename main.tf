13f43de7b9019bbfbe24e95963bb2a06854aafc1
Lakshika1210 <lakshikamalik05@gmail.com>
2026-04-23 14:37:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
 resource "aws_s3_bucket" "this" {
   bucket = var.bucket_name
 }
